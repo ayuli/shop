e930f5d8e13f64a1cc85746e1dc0cd9cb3751d98
ayuli <45835858+ayuli@users.noreply.github.com>
2019-01-24 11:30:11 +0800

    缓存

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 use App\Model\GoodsModel;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 
 class IndexController extends Controller
 {
@@ -21,17 +22,34 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $goods = GoodsModel::paginate(3);
+        $redis_goods_key = 'h_goods_info';
+        $goods_info = Redis::hGetAll($redis_goods_key);
+        if($goods_info){
+            echo 'Redis';echo '</br>';
+
+            print_r($goods_info);
+        }else{
+            echo 'Mysql';
+            $goods_info = GoodsModel::paginate(3);
+            print_r($goods_info);
+            //写入缓存
+            $rs = Redis::hmset($redis_goods_key,$goods_info);
+            //设置缓存过期时间
+            Redis::expire($redis_goods_key,10);
+        }
+
+        die;
+
 
         //商品不存在
-        if(empty($goods)){
+        if(empty($goods_info)){
             header('Refresh:2;url=/');
             echo '商品不存在,正在跳转至首页';
             exit;
         }
 
         $data = [
-            'goods' => $goods
+            'goods' => $goods_info
         ];
         return view('goods.index',$data);
     }
