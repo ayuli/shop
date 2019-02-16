2c0f8c0296482668d67e85eaf9fcb75e79e2d96f
yuxinwei <649160717@qq.com>
2019-02-16 15:29:57 +0800

    微信

--- routes/web.php
+++ routes/web.php
@@ -119,3 +119,11 @@ Route::post('/goods/upload/pdf','Goods\IndexController@uploadPDF');
 //redis 在线购票
 Route::get('/movie','Movie\IndexController@index');
 Route::get('/movie/buy/{pos}/{status}','Movie\IndexController@buy');
+
+
+//微信
+Route::get('/weixin/test','Weixin\WeixinController@test');
+Route::get('/weixin/valid','Weixin\WeixinController@validToken');
+Route::get('/weixin/valid1','Weixin\WeixinController@validToken1');
+Route::post('/weixin/valid1','Weixin\WeixinController@wxEvent');        //接收微信服务器事件推送
+Route::post('/weixin/valid','Weixin\WeixinController@validToken');
